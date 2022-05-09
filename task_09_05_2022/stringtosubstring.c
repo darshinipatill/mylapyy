32c8295940f78cc9fb3f99294a4c31505c1318fd
darshinipatill <104495857+darshinipatill@users.noreply.github.com>
2022-05-09 12:56:29 +0530

    first commit

---
+++ task_09_05_2022/stringtosubstring.c
@@ -0,0 +1,31 @@
+//Check if string start with the substring  
+#include<stdio.h>
+#include <string.h>
+#define true  1
+#define false 0
+//This function checks if the given string starts with substring or not 
+//it returns 1 if yes else 0
+int isStartWithSubstr(char *string, char *substring){
+    int substringLength = strlen(substring);
+    for(int i = 0; i < substringLength; i++)
+    {
+        if(string[i] != substring[i]){
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{   
+    // Input string and substring
+    char input_string[] = "Rajaram";
+    char substring[] = "Raj";
+    
+    if(isStartWithSubstr(input_string, substring)){
+        printf("The String is start with substring");
+    }
+       printf("The String is NOT start with %s",substring);
+    }
+      
+    return 0;
+}
